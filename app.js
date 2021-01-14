9deeb355d8db6c01c45a6ba5435145516a40d834
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-14 08:13:52 -0800

    implement adding note

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
 const chalk = require('chalk');
-const getNotes = require('./notes');
+const yargs = require('yargs');
+const notes = require('./notes');
 
-const msg = getNotes();
+// yargs.command;
+yargs.version('1.1.0');
 
-console.log(msg);
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
 
-console.log(chalk.blue.inverse('Success!'));
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing the note!');
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function() {
+    console.log('Listing out all notes!');
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log('Reading a note!');
+  }
+});
+
+yargs.parse();
